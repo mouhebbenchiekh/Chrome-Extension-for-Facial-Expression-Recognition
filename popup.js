f19da9630dd43d3394f47762fe637b2a1ef9f4c7
DorraGara <dorra.gara@insat.u-carthage.tn>
2022-06-05 15:17:33 +0200

    ui + clean up

--- popup.js
+++ popup.js
@@ -1,12 +1,10 @@
-
-
-
-
-  function elementFunc({sad,happy,angry,neutral,surprised,fear}){
-    return `
-   
+function face_analysis_item({ key, image_source, Emotion, angry, fear, happy, neutral, surprised, sad }) {
+  return `    
+  <div class="face_analysis_element">
+  <img class="outputImage" src="${image_source}"/>
+  <div class="element" id="element${key}">
   <div class="container">
-    <h2>Fear</h2>
+    <h2>${Emotion}</h2>
     <div class="row">
       <span class="emotionItem">Angry</span>
       <div class="progress ">
@@ -54,122 +52,94 @@
 
       </div>
     </div>
- 
-    `
-  }
- 
- 
+  </div>
+ </div>
+ `
+}
 
-
- /* ends here*/
-
-let button= document.getElementById('capture');
-let buttonCrop= document.getElementById('crop');
-let video= document.getElementById('video');
-let element= document.getElementById('element');
+let button = document.getElementById('capture');
+let buttonCrop = document.getElementById('crop');
+let video = document.getElementById('video');
+let whole_container = document.querySelector(".whole_container");
 let interval
-let isSending=false
-let first=true
-button.addEventListener('click',async ()=>{
-  if(!isSending){
-    video.style.display="block";
-    element.style.display="block";
+let isSending = false
+let first = true
+button.addEventListener('click', async () => {
+  if (!isSending) {
+    video.style.display = "block";
+    button.innerText = "Pause";
     try {
-      let model 
-      if (first){
+      let model
+      if (first) {
         model = await tf.loadLayersModel('http://localhost:3000/model.json');
-        first=false;
+        first = false;
       }
       Promise.all([
         faceapi.nets.tinyFaceDetector.loadFromUri('/models'),]).then(capture(model));
-      
 
-      
     } catch (error) {
-      console.log({error});
-      
+      console.log({ error });
+
     }
-  }else{
+  } else {
     clearInterval(interval)
   }
   isSending = !isSending
-
-
-    
-      
 })
 
-function capture(model){
-
-chrome.tabCapture.capture({audio:false,video:true},(stream)=>{
-    
+function capture(model) {
+  chrome.tabCapture.capture({ audio: false, video: true }, (stream) => {
     video.srcObject = stream;
     video.addEventListener('play', () => {
-         const canvas = faceapi.createCanvasFromMedia(video)
-        document.body.append(canvas)
-        const displaySize = { width: video.width, height: video.height }
-        faceapi.matchDimensions(canvas, displaySize) 
-        interval = setInterval(async () => {
-          
-          if(isSending){
-            const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
-            extractFaceFromBox(video, detections[0]._box,model)
-           const resizedDetections = faceapi.resizeResults(detections, displaySize)
-           canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-           faceapi.draw.drawDetections(canvas, resizedDetections)
-           
-          }
-         
-        }, 300)
+      const canvas = faceapi.createCanvasFromMedia(video)
+      document.body.append(canvas)
+      const displaySize = { width: video.width, height: video.height }
+      faceapi.matchDimensions(canvas, displaySize)
+      interval = setInterval(async () => {
+        if (isSending) {
+          const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+          whole_container.innerHTML = "";
+          extractFaceFromBox(video, detections[0]._box, model)
+          const resizedDetections = faceapi.resizeResults(detections, displaySize)
+          canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+          faceapi.draw.drawDetections(canvas, resizedDetections)
 
-      })
-
-      
-
-    
-    
-})
-
+        }
+      }, 300)
+    })
+  })
 }
 
-let outputImage = document.getElementById('outputImage')
-async function extractFaceFromBox(inputImage, box,model){ 
+async function extractFaceFromBox(inputImage, box, model) {
   const regionsToExtract = [
-      new faceapi.Rect( box.x, box.y , box.width , box.height)
+    new faceapi.Rect(box.x, box.y, box.width, box.height)
   ]
-                      
+
   let faceImages = await faceapi.extractFaces(inputImage, regionsToExtract)
-  
-  if(faceImages.length == 0){
-      console.log('Face not found')
+  console.log(faceImages);
+  if (faceImages.length == 0) {
+    console.log('Face not found')
   }
-  else
-  {
-      faceImages.forEach((cnv,key) =>{      
-          outputImage.src = cnv.toDataURL(); 
-          
-           let pixelImage= tf.browser.fromPixels(cnv);
-           let resized=tf.image.resizeBilinear(pixelImage,[48,48],false,false);
-           let greyImage=resized.mean(2);
-           let detect=tf.expandDims(tf.reshape(greyImage,[48,48,1]),0)
-           
-            // for example
-             let can =document.getElementById('can');
-            tf.browser.toPixels(tf.reshape(detect,[48,48]),can);
-            let prediction = model.predict(detect).dataSync();
-            let pred = prediction.map(val => Math.round((val*100)));
-            const [angry,fear,happy,neutral,sad,surprised] = pred;
-           
-            document.querySelector("#element").innerHTML=elementFunc({angry: angry,fear:fear,happy:happy,neutral:neutral,sad:sad,surprised:surprised})
-            console.log("lenna naffichiw fel element bars ...")
-            console.log(document.querySelector("#element"))
-           
-              $(".progress .progress-bar").css("width", function () {
-                return $(this).attr("aria-valuenow") + "%";
-              });
-              
-                      
-              
-      })
-  }   
-} 
+  else {
+    faceImages.forEach((cnv, key) => {
+      let pixelImage = tf.browser.fromPixels(cnv);
+      let resized = tf.image.resizeBilinear(pixelImage, [48, 48], false, false);
+      let greyImage = resized.mean(2);
+      let detect = tf.expandDims(tf.reshape(greyImage, [48, 48, 1]), 0)
+      let prediction = model.predict(detect).dataSync();
+      let pred = prediction.map(val => Math.round((val * 100)));
+      const [angry, fear, happy, neutral, sad, surprised] = pred;
+      let Emotion = pred.indexOf(Math.max(...pred));
+      const emotionList = ["Angry", "Fear", "Happy", "Neutral", "Sad", "Surprise"];
+      var item = document.createElement("div");
+      item.innerHTML = face_analysis_item({ key: key, image_source: cnv.toDataURL(), Emotion: emotionList[Emotion], angry: angry, fear: fear, happy: happy, neutral: neutral, sad: sad, surprised: surprised })
+      whole_container.appendChild(item);
+      $(".progress .progress-bar").css("width", function () {
+        return $(this).attr("aria-valuenow") + "%";
+      });
+    })
+  }
+}
+
+
+
